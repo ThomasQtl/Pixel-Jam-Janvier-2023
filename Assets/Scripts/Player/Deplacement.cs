15b0dcbe33ac0ee34ab3b3215c575067d874a130
ThomasQtl <thomas.quentel2@gmail.com>
2023-01-14 16:05:27 +0100

    Player bouge

---
+++ Assets/Scripts/Player/Deplacement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    public Animator _animator;
+    public SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>(); 
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.flipX = true;
+    }
+
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+
+        /*if (!Mathf.Approximately(0, movement)) {
+            transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }*/
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) 
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        Flip(movement);
+        float characterVelocity = Mathf.Abs(movement);
+        print(characterVelocity);
+        _animator.SetFloat("Speed", characterVelocity);
+    }
+
+    void Flip(float movement)  
+    {
+        if (movement > 0.08f) 
+        {
+            _spriteRenderer.flipX = false;
+        } 
+        else if (movement < -0.08f) 
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+}
